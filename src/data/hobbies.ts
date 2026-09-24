d00443ce53735c4628cbfb4541b6a2125081ee9b
Snaggy <ahmadshami48@gmail.com>
2026-09-24 07:27:08 +0200

    fixed invasive CV download and added hobbies

---
+++ src/data/hobbies.ts
@@ -0,0 +1,10 @@
+export const Hobbies: string[] = [
+  "Playing CTFs",
+  "Bug Bounty hunting",
+  "Learning electric guitar (Stratocaster specifically)",
+  "Swimming",
+  "Playing Tennis",
+  "Reading Books (Novels, psychology science, strategy science...)",
+  "Listening to Podcasts (psychology and well being, true crime, history...)",
+  "Video games (Platformers, Retro emulated games, strategy base defense...)",
+];
